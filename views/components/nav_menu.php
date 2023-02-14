0cfad84a4552024c3a804eb20d3bb83344d51549
CookieMog <31330690+CookieMog@users.noreply.github.com>
2023-02-14 14:55:22 -0400

    transition homepage mobile to desktop

---
+++ views/components/nav_menu.php
@@ -0,0 +1,11 @@
+<div class="NavMenuHeader">
+    <div id="AdminLogo"><img src="" alt="NavLogo"></div>
+</div>
+<div class="NavMenu">
+    <ul class="Options">
+        <li class="menu_element"><a href="index.php?action=home" class="menu_link">Accueil</a></li>
+        <li class="menu_element"><a href="index.php?action=gallery" class="menu_link">Gallerie</a></li>
+        <li class="menu_element"><a href="index.php?action=contact" class="menu_link">Contact</a></li>
+
+    </ul>
+</div>
--- views/home.php
+++ views/home.php
@@ -25,21 +25,29 @@
                         excepturi dolorem eum internos facere nam omnis architecto sit
                         aliquam sapiente. Ut omnis obcaecati id provident quas sed quia
                         expedita vel cumque exercitationem.
+                        <br />
+                        Lorem ipsum dolor sit amet. Ut praesentium tenetur et voluptas
+                        velit eos exercitationem voluptates et eaque quaerat a enim culpa?
+                        Sit veniam nihil sit beatae architecto et dolore voluptas quo
+                        excepturi dolorem eum internos facere nam omnis architecto sit
+                        aliquam sapiente. Ut omnis obcaecati id provident quas sed quia
+                        expedita vel cumque exercitationem.
+                        <br />
+                        Lorem ipsum dolor sit amet. Ut praesentium tenetur et voluptas
+                        velit eos exercitationem voluptates et eaque quaerat a enim culpa?
+                        Sit veniam nihil sit beatae architecto et dolore voluptas quo
+                        excepturi dolorem eum internos facere nam omnis architecto sit
+                        aliquam sapiente. Ut omnis obcaecati id provident quas sed quia
+                        expedita vel cumque exercitationem.
                     </p>
                 </div>
             </div>
 
         </div>
         <div class="column2">
-
-
-
+            <?php include("components/nav_menu.php") ?>
         </div>
     </div>
-
-
-
-
 
     <!-- Code injected by live-server -->
     <?php include("components/footer.php") ?>
