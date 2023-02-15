c6e5a4eb0fc9b171fac5fd9485ba79f49c4e29d5
CookieMog <31330690+CookieMog@users.noreply.github.com>
2023-02-15 13:02:29 -0400

    adjustments desktop mobile with placeholder images

--- views/components/nav_menu.php
+++ views/components/nav_menu.php
@@ -1,11 +1,12 @@
 <div class="NavMenuHeader">
-    <div id="AdminLogo"><img src="" alt="NavLogo"></div>
+    <div id="AdminLogo"><img src="https://via.placeholder.com/200x100" alt="NavLogo"></div>
 </div>
 <div class="NavMenu">
     <ul class="Options">
         <li class="menu_element"><a href="index.php?action=home" class="menu_link">Accueil</a></li>
         <li class="menu_element"><a href="index.php?action=gallery" class="menu_link">Gallerie</a></li>
         <li class="menu_element"><a href="index.php?action=contact" class="menu_link">Contact</a></li>
+        <li class="menu_element"><a href="index.php?action=connection" class="menu_link">Se Connecter</a></li>
 
     </ul>
 </div>
--- views/home.php
+++ views/home.php
@@ -6,13 +6,19 @@
 <body>
     <?php include("components/header.php") ?>
     <div class="containers">
+        <div class="mobilemenubar">
+            <?php include("components/dropdown_menu.php") ?>
+        </div>
         <div class="column1">
-            <div class="mobilemenubar">
-                <?php include("components/dropdown_menu.php") ?>
-            </div>
+            <?php include("components/nav_menu.php") ?>
+
+        </div>
+
+
+        <div class="column2">
             <div class="Introduction">
                 <div class="Photo">
-                    <img src="" alt="AlexisPortrait">
+                    <img src="https://via.placeholder.com/320x400" alt="AlexisPortrait">
                 </div>
                 <h1>
                     Bienvenue sur mon Portfolio
@@ -26,12 +32,35 @@
                         aliquam sapiente. Ut omnis obcaecati id provident quas sed quia
                         expedita vel cumque exercitationem.
                         <br />
+                        <br />
                         Lorem ipsum dolor sit amet. Ut praesentium tenetur et voluptas
                         velit eos exercitationem voluptates et eaque quaerat a enim culpa?
                         Sit veniam nihil sit beatae architecto et dolore voluptas quo
                         excepturi dolorem eum internos facere nam omnis architecto sit
                         aliquam sapiente. Ut omnis obcaecati id provident quas sed quia
                         expedita vel cumque exercitationem.
+                        <br />
+                        <br />
+                        Lorem ipsum dolor sit amet. Ut praesentium tenetur et voluptas
+                        velit eos exercitationem voluptates et eaque quaerat a enim culpa?
+                        Sit veniam nihil sit beatae architecto et dolore voluptas quo
+                        excepturi dolorem eum internos facere nam omnis architecto sit
+                        aliquam sapiente. Ut omnis obcaecati id provident quas sed quia
+                        expedita vel cumque exercitationem.
+                        <br />
+                        <br />
+                        Lorem ipsum dolor sit amet. Ut praesentium tenetur et voluptas
+                        velit eos exercitationem voluptates et eaque quaerat a enim culpa?
+                        Sit veniam nihil sit beatae architecto et dolore voluptas quo
+                        excepturi dolorem eum internos facere nam omnis architecto sit
+                        aliquam sapiente. Ut omnis obcaecati id provident quas sed quia
+                        expedita vel cumque exercitationem. Lorem ipsum dolor sit amet. Ut praesentium tenetur et voluptas
+                        velit eos exercitationem voluptates et eaque quaerat a enim culpa?
+                        Sit veniam nihil sit beatae architecto et dolore voluptas quo
+                        excepturi dolorem eum internos facere nam omnis architecto sit
+                        aliquam sapiente. Ut omnis obcaecati id provident quas sed quia
+                        expedita vel cumque exercitationem.
+                        <br />
                         <br />
                         Lorem ipsum dolor sit amet. Ut praesentium tenetur et voluptas
                         velit eos exercitationem voluptates et eaque quaerat a enim culpa?
@@ -44,9 +73,7 @@
             </div>
 
         </div>
-        <div class="column2">
-            <?php include("components/nav_menu.php") ?>
-        </div>
+    </div>
     </div>
 
     <!-- Code injected by live-server -->
