d864d08a6f59890561c903e44eb5c09d20640a96
CookieMog <31330690+CookieMog@users.noreply.github.com>
2023-02-15 20:47:31 -0400

    changements CSS, début bannière

--- views/components/header.php
+++ views/components/header.php
@@ -1,7 +1,7 @@
 <header>
     <div id="banner">
-        <img src="" alt="imageBanniere1" />
-        <img src="" alt="imageBanniere2" />
-        <img src="" alt="imageBanniere3" />
+        <img src="https://via.placeholder.com/356x244" alt="imageBanniere1" />
+        <img src="https://via.placeholder.com/356x244" alt="imageBanniere2" />
+        <img src="https://via.placeholder.com/356x244" alt="imageBanniere3" />
     </div>
 </header>
